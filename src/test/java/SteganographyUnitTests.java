0db2ed8c8a6a6e5673a263b878b2d2d9816f3348
vpavlyshen <Volodymyr.Pavlyshen@onseo.biz>
2023-06-14 18:55:17 +0300

    fix class name bug

--- src/test/java/SteganographyUnitTests.java
+++ src/test/java/SteganographyUnitTests.java
@@ -10,7 +10,7 @@ import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertNotNull;
 import static org.testng.Assert.assertTrue;
 
-public class SteganographyUnitTest {
+public class SteganographyUnitTests {
 
     @DataProvider(name = "testMessages")
     public Object[][] testDataMessages() {
